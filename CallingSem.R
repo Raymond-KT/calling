f7f815be7eb0f5a51d1b59da7ce0b284028d7638
Raymond.Kim <yuaye.kt@gmail.com>
2020-04-01 09:40:53 +0900

    CFA add

--- CallingSem.R
+++ CallingSem.R
@@ -40,7 +40,6 @@ library(dtplyr)
 
 #### (1) 변수명 지정 ####
 
-class(Raw)
 Raw_df <- data.frame(Raw,stringsAsFactors = FALSE)
 Raw_df <- Raw_df[,-1:-6]
 Raw_df <- Raw_df[-1,]
@@ -105,14 +104,13 @@ library(psych)
 library(GPArotation)
 library(mvtnorm)
 
-EFA <- Raw_df[,c("OI1","OI2","OI3","OI4","OI5","JaC1","JaC2","JaC3","JaC4","JaC5","JaC6","JaC7","JS1","JS2","JS3","JS4","JS5","JS6","JP1","JP2","JP3","JP4","JP5","JP6_R","JP7_R")]
+EFA <- Raw_df[,c("OI1","OI2","OI3","OI4","OI5","JaC1","JaC2","JaC3","JaC4","JaC5","JaC6","JaC7",
+                 "JS1","JS2","JS3","JS4","JS5","JS6","JP1","JP2","JP3","JP4","JP5","JP6_R","JP7_R")]
 
 EFA <- apply((EFA),1,as.numeric)
 EFA%>% cor() %>% eigen()
 
-EFA_P<- principal(EFA,rotate = 'none'
-                  
-                  )
+EFA_P<- principal(EFA,rotate = 'none')
 
 
 
@@ -124,16 +122,64 @@ abline(v=20)
 ggplot(EFA,aes(x=EFA_P$values))+geom_point()
 
 EFA_s <- scale(EFA)
-
 fac1 <- factanal(EFA_s,4)
-  
 printf(digit=2,sort=T)
 
+
+# 탐색적 요인분석은, 요인 개수가 거의 20개가 나옴
+# 이미 학술적으로 요인이 검증되었으므로, 확인적 요인분석을 진행함
 
 
 
 # 1-2. 확인적 요인분석 
 
+#잠재변수 지정
+
+#model 설정
+HS.Model <- 'Organization_Identity = ~OI1+OI2+OI3+OI4+OI5
+            Job_As_Calling= ~JaC1+JaC2+JaC3+JaC4+JaC5+JaC6+JaC7
+            Job_Satisfaction= ~JS1+JS2+JS3+JS4+JS5+JS6
+            Job_Performance= ~JP1+JP2+JP3+JP4+JP5+JP6_R+JP7_R'
+
+#CFA(확증적 요인분석)
+
+fit <- cfa(HS.Model,data=Raw_df)
+
+#확증적 요인분석 결과 추출
+
+# #lavaan 패키지 버전, 해가 수렴했는지 여부와 반복 횟수, 관측값 개수, 모델추정 방법,
+#모델적합도 검정결과(카이제곱 검정통계량, 자유도, p-값).
+# 다음 섹션은 Chisq 적합도 지표 이외의 추가적인 적합도 지표
+# 마지막 섹션은 모수추정치를 담고 있다. 잠재변수, 공분산, 잔차분산의 순서로 모수추 청치
+
+summary(fit, fit.measures=TRUE, standardized = TRUE)
+
+#모수 추정치 추출 
+parameterEstimates(fit, standardized=TRUE)
+
+# 도표로 보기
+windows(record=T)
+diagram<-semPlot::semPaths(fit,
+                           whatLabels="std", intercepts=FALSE, style="lisrel",
+                           nCharNodes=0, nCharEdges=0,
+                           curveAdjacent = TRUE,title=TRUE, layout="tree2",curvePivot=TRUE)
+
+#요약 테이블로 만들기
+
+library(knitr)
+options(knitr.kable.NA="")
+
+parameterEstimates(fit, standardized=TRUE) %>%
+   filter(op=="=~") %>%
+  mutate(stars=ifelse(pvalue < 0.001, "***",ifelse(pvalue < 0.01, "**",ifelse(pvalue < 0.05, "*", "")))) %>%
+  select("Latent Factor"=lhs, Indicator=rhs, B=est, SE=se,
+  Z=z, "p-value"=pvalue, Sig.=stars, Beta=std.all) %>%
+  kable(digits=3, format="pandoc", caption="Factor Loadings")
+
+residuals(fit,type="cor")$cov
 
 
 
+
+library(semTools)
+install.packages('semtools')
